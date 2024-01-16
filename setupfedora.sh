4918dc28bfaecdbe14686cc41673906ec99aff1f
Ali Kirici <mail.contact.kali@gmail.com>
2024-01-16 17:09:23 +0100

    stored URLs in variables, formatted font for output messages for better readability

--- setupfedora.sh
+++ setupfedora.sh
@@ -2,71 +2,79 @@
 
 set -e
 
-check_for_root() 
+info()
+{
+    echo -e "\e[1;4;32;40m${*}\e[0m"
+}
+
+URL_FLATHUB_REPO="https://flathub.org/repo/flathub.flatpakrepo"
+URL_RPMFUSION_FREE="https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
+URL_RPMFUSION_NONFREE="https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+URL_MICRO="https://getmic.ro"
+
+check_for_root()
 {
 if [ "$EUID" -ne 0 ]; then
-    echo -e "This script cannot run without root privileges.\nIt intends to configure the DNF package manager\nto enable parallel downloads and install\nthird party repositories, Google Chrome, VSCode\nand the Micro Texteditor. It will further\ninstall or update Python and Golang.\nIt will also update packages inbetween.\nIt is written to perform a quick setup\non a new installation of Fedora Linux Workstation.\nRun this script as root or contact your SysAdmin."
+    info "This script cannot run without root privileges.\nIt intends to configure the DNF package manager\nto enable parallel downloads and install\nthird party repositories, Google Chrome, VSCode\nand the Micro Texteditor. It will further\ninstall or update Python and Golang.\nIt will also update packages inbetween.\nIt is written to perform a quick setup\non a new installation of Fedora Linux Workstation.\nRun this script as root or contact your SysAdmin."
     exit 1
 fi
 }
 
-configure_dnf() 
+configure_dnf()
 {
-echo "Configuring DNF to enable 15 parallel downloads."
+info "Configuring DNF to enable 15 parallel downloads."
 if grep -q "max_parallel_downloads" /etc/dnf/dnf.conf; then
-    echo -e "DNF is already configured for parallel downloads.\nConfiguration will be overwritten to enable 15 parallel downloads."
+    info "DNF is already configured for parallel downloads.\nConfiguration will be overwritten to enable 15 parallel downloads."
     sed -i '/^max_parallel_downloads=/c\max_parallel_downloads=15' /etc/dnf/dnf.conf
 else
     echo "max_parallel_downloads=15" | tee -a /etc/dnf/dnf.conf >/dev/null
-    echo "Configured DNF for 15 parallel downloads."
+    info "Configured DNF for 15 parallel downloads."
 fi
 }
 
 install_flatpak()
 {
 if ! command -v flatpak &>/dev/null; then
-    echo "Flatpak is not installed. Installing..."
+    info "Flatpak is not installed. Installing..."
     dnf install flatpak -y
 fi
 }
 
 install_flathub_repos()
 {
-    echo "Installing Flathub repositories if not already installed."
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    info "Installing Flathub repositories if not already installed."
+    flatpak remote-add --if-not-exists flathub $URL_FLATHUB_REPO
 }
 
-install_rpmfusion_repos() 
+install_rpmfusion_repos()
 {
-    echo "Installing RPM Fusion Free and Non-Free repositories."
-    dnf install -y \
-    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
-    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
+    info "Installing RPM Fusion Free and Non-Free repositories."
+    dnf install -y "$URL_RPMFUSION_FREE" "$URL_RPMFUSION_NONFREE"
 }
 
 update_packages()
 {
-    echo "UPDATING PACKAGES."
+    info "UPDATING PACKAGES."
     dnf update -y
     flatpak update -y
 }
 
 install_chrome()
 {
-    echo "Installing Google Chrome from the flathub repository."
+    info "Installing Google Chrome from the flathub repository."
     flatpak install -y flathub com.google.Chrome
 }
 
 install_micro()
 {
-    echo "Installing Micro Texteditor."
-    curl https://getmic.ro | bash
+    info "Installing Micro Texteditor."
+    curl $URL_MICRO | bash
     mv micro /usr/local/bin
 }
 
 install_vsc()
 {
-    echo "Installing Microsoft Visual Studio Code."
+    info "Installing Microsoft Visual Studio Code."
     rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
     dnf check-update
@@ -76,19 +84,19 @@ install_vsc()
 install_python()
 {
 if command -v python3 &>/dev/null; then
-    echo "Python is already installed."
+    info "Python is already installed."
 else
-    echo "Python not found. Installing..."
+    info "Python not found. Installing..."
     dnf install python3 -y
 fi
 }
 
 install_golang()
 {
-if command -v go &>/dev/null; then 
-    echo "Golang is already installed."
+if command -v go &>/dev/null; then
+    info "Golang is already installed."
 else
-    echo "Golang not found. Installing..."
+    info "Golang not found. Installing..."
     dnf install golang -y
 fi
 }
@@ -111,4 +119,4 @@ main()
 
 main
 
-echo "Setup complete."
+info "Setup complete."
