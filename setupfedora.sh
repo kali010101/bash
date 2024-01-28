3767c7bb0c5284bb214fa078dce00c61679d56f0
Ali Kirici <mail.contact.kali@gmail.com>
2024-01-28 02:28:27 +0100

    stdout sent to nulldev, put functions in guard clauses, standardized/corrected indentation

--- setupfedora.sh
+++ setupfedora.sh
@@ -14,62 +14,75 @@ URL_MICRO="https://getmic.ro"
 
 check_for_root()
 {
-if [ "$EUID" -ne 0 ]; then
-    info "This script cannot run without root privileges.\nIt intends to configure the DNF package manager\nto enable parallel downloads, add repositories\nfor Flathub and RPM Fusion (Free and Non-Free)\nand install Google Chrome, VSCode, Virt-Manager\nand the text editor Micro. It will further update\nor install Python and Golang and uninstall apps\nconsidered to be bloatware. Dependencies which\nmight become obsolete as a result of this will\nalso be deleted. Run this script as root or with\ntemporarily elevated privileges (superuser)."
-    exit 126
-fi
+    if [ "$EUID" -ne 0 ]; then
+        info "This script cannot run without root privileges.\nIt intends to configure the DNF package manager\nto enable parallel downloads, add repositories\nfor Flathub and RPM Fusion (Free and Non-Free)\nand install Google Chrome, VSCode, Virt-Manager\nand the text editor Micro. It will further update\nor install Python and Golang and uninstall apps\nconsidered to be bloatware. Dependencies which\nmight become obsolete as a result of this will\nalso be deleted. Run this script as root or with\ntemporarily elevated privileges (superuser)."
+        exit 126
+    fi
 }
 
 configure_dnf()
 {
-info "Configuring DNF to enable 15 parallel downloads."
-if grep -q "max_parallel_downloads" /etc/dnf/dnf.conf; then
-    info "DNF is already configured for parallel downloads.\nConfiguration will be overwritten to enable 15 parallel downloads."
-    sed -i '/^max_parallel_downloads=/c\max_parallel_downloads=15' /etc/dnf/dnf.conf
-else
-    echo "max_parallel_downloads=15" | tee -a /etc/dnf/dnf.conf >/dev/null
+    DNFCONFIG_PATH="/etc/dnf/dnf.conf"
+    info "Configuring DNF to enable 15 parallel downloads."
+    if grep -q "max_parallel_downloads" $DNFCONFIG_PATH; then
+        info "DNF is already configured for parallel downloads.\nConfiguration will be overwritten to enable 15 parallel downloads."
+        sed -i '/^max_parallel_downloads=/c\max_parallel_downloads=15' $DNFCONFIG_PATH
+    else
+    echo "max_parallel_downloads=15" | tee -a $DNFCONFIG_PATH >/dev/null
     info "Configured DNF for 15 parallel downloads."
-fi
+    fi
 }
 
 install_flatpak()
 {
-if ! command -v flatpak &>/dev/null; then
-    info "Flatpak is not installed. Installing..."
-    dnf install -y flatpak
-fi
+    if ! command -v flatpak >/dev/null; then
+        info "Flatpak is not installed. Installing..."
+        if ! dnf install -y flatpak >/dev/null; then
+            info "Flatpak could not be installed."
+        fi
+        info "Flatpak has been installed successfully."
+    fi
 }
 
 install_flathub_repos()
 {
     info "Installing Flathub repositories if not already installed."
-    flatpak remote-add --if-not-exists flathub "$URL_FLATHUB_REPO"
+    flatpak remote-add --if-not-exists flathub "$URL_FLATHUB_REPO" >/dev/null
 }
 
 install_rpmfusion_repos()
 {
     info "Installing RPM Fusion Free and Non-Free repositories."
-    dnf install -y "$URL_RPMFUSION_FREE" "$URL_RPMFUSION_NONFREE"
+    if ! dnf install -y "$URL_RPMFUSION_FREE" "$URL_RPMFUSION_NONFREE" >/dev/null; then
+        info "RPM Fusion repositories could not be installed."
+    fi
+    info "RPM Fusion repositories have been installed."
 }
 
 update_packages()
 {
     info "UPDATING PACKAGES."
-    dnf update -y
-    flatpak update -y
+    dnf update -y >/dev/null
+    flatpak update -y >/dev/null
 }
 
 install_chrome()
 {
     info "Installing Google Chrome from the flathub repository."
-    flatpak install -y flathub com.google.Chrome
+    if ! flatpak install -y flathub com.google.Chrome >/dev/null; then
+        info "Google Chrome could not be installed."
+    fi
+    info "Google Chrome has been installed successfully."
 }
 
 install_micro()
 {
     info "Installing Micro Texteditor."
-    curl "$URL_MICRO" | bash
+    curl "$URL_MICRO" | bash >/dev/null
     mv micro /usr/local/bin
+    if ! command -v micro >/dev/null; then
+        info "The text editor Micro could either\nnot be installed or moved into a directory\nassigned to the PATH variable."
+    fi
 }
 
 install_vsc()
@@ -77,45 +90,53 @@ install_vsc()
     info "Installing Microsoft Visual Studio Code."
     rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-    dnf check-update
-    dnf install -y code
+    dnf check-update >/dev/null
+    if ! dnf install -y code >/dev/null; then
+        info "Microsoft Visual Studio Code could not be installed."
+    fi
+    info "Microsoft Visual Studio Code has been installed successfully."
 }
 
 install_python()
 {
-if command -v python3 &>/dev/null; then
-    info "Python is already installed."
-else
+if ! command -v python3 >/dev/null; then
     info "Python not found. Installing..."
-    dnf install -y python3
+    if ! dnf install -y python3 >/dev/null; then
+        info "Python could not be installed."
+    fi
+    info "Python has been installed successfully."
 fi
 }
 
 install_golang()
 {
-if command -v go &>/dev/null; then
-    info "Golang is already installed."
-else
+if ! command -v go >/dev/null; then
     info "Golang not found. Installing..."
-    dnf install -y golang
+    if ! dnf install -y golang >/dev/null; then
+        info "Golang could not be installed."
+    fi
+    info "Golang has been installed successfully."
 fi
 }
 
 install_virt_manager()
 {
-    dnf install -y virt-manager
+    info "Installing Virt-Manager."
+    if ! dnf install -y virt-manager >/dev/null; then
+        info "Virt-Manager could not be installed."
+    fi
+    info "Virt-Manager has been installed successfully."
 }
 
 remove_gnome_bloat()
 {
     info "Uninstalling bloatware."
-    if dnf remove -y gnome-maps gnome-tour gnome-weather gnome-boxes gnome-contacts; then
-        info "Bloatware successfully uninstalled."
-    else
+    if ! dnf remove -y gnome-maps gnome-tour gnome-weather gnome-boxes gnome-contacts >/dev/null; then
         info "Error: failed to uninstall bloatware."
         exit 1
     fi
-    dnf autoremove -y
+    dnf autoremove -y >/dev/null
+    info "Bloatware has been uninstalled successfully."
 }
 
 main()
